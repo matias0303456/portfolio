00e6a0fce712cc18834db2ab623215dd5215ae9c
Matias Ramos <matiasramos@gmail.com>
2022-08-30 19:49:23 -0300

    estilos perfil

--- src/pages/Home/components/Profile.js
+++ src/pages/Home/components/Profile.js
@@ -16,41 +16,49 @@ export default function Profile() {
             <h2>
                 Perfil
             </h2>
-            <p>
-                Mi trabajo se centra en el desarrollo y mantenimiento de aplicaciones web
-                con tecnologías del lado del cliente y del lado del servidor.
-                Así también, cuento con conocimientos en herramientas de analítica,
-                en buenas prácticas de desarrollo y documentación, en seguridad
-                (principalmente utilización de JWT) y en optimización de accesibilidad
-                y rendimiento para SEO.
-            </p>
-            <p>
-                En frontend trabajo con las tecnologías básicas: HTML, CSS y JavaScript.
-                Además, implemento librerías y frameworks, principalmente React JS
-                a un nivel avanzado. En CSS, también trabajo con Bootstrap y Tailwind.
-            </p>
-            <p>
-                En backend, trabajo con Node JS, entorno de ejecución de JavaScript, y con
-                Laravel, framework de PHP. Como sistema gestor de bases de datos uso MySQL.
-                Para conectar APIs con bases de datos, utilizo ORMs como Prisma o Eloquent,
-                o directamente el lenguaje SQL.
-            </p>
-            <p>
-                Mi conocimiento de JavaScript y de PHP me permite también utilizar herramientas
-                de código de terceros, librerías que me permiten implementar determinadas
-                funcionalidades de forma más eficiente y rápida, como NPM y Composer.
-            </p>
-            <AiFillHtml5 />
-            <IoLogoCss3 />
-            <IoLogoJavascript />
-            <SiBootstrap />
-            <SiTailwindcss />
-            <DiReact />
-            <FaNode />
-            <FaPhp />
-            <SiPrisma />
-            <FaLaravel />
-            <SiMysql />
+            <div className='profile_container'>
+                <div className='logo_container'>
+                    <AiFillHtml5 id='html_logo' className='logo' />
+                    <DiReact id='react_logo' className='logo' />
+                    <FaPhp id='php_logo' className='logo' />
+                    <SiBootstrap id='bs_logo' className='logo' />
+                    <SiMysql id='mysql_logo' className='logo' />
+                </div>
+                <div className='text_container'>
+                    <p>
+                        Mi trabajo se centra en el desarrollo y mantenimiento de aplicaciones web
+                        con tecnologías del lado del cliente y del lado del servidor.
+                        Así también, cuento con conocimientos en herramientas de analítica,
+                        en buenas prácticas de desarrollo y documentación, en seguridad
+                        (principalmente utilización de JWT) y en optimización de accesibilidad
+                        y rendimiento para SEO.
+                    </p>
+                    <p>
+                        En frontend trabajo con las tecnologías básicas: HTML, CSS y JavaScript.
+                        Además, implemento librerías y frameworks, principalmente React JS
+                        a un nivel avanzado. En CSS, también trabajo con Bootstrap y Tailwind.
+                    </p>
+                    <p>
+                        En backend, trabajo con Node JS, entorno de ejecución de JavaScript, y con
+                        Laravel, framework de PHP. Como sistema gestor de bases de datos uso MySQL.
+                        Para conectar APIs con bases de datos, utilizo ORMs como Prisma o Eloquent,
+                        o directamente el lenguaje SQL.
+                    </p>
+                    <p>
+                        Mi conocimiento de JavaScript y de PHP me permite también utilizar herramientas
+                        de código de terceros, librerías que me permiten implementar determinadas
+                        funcionalidades de forma más eficiente y rápida, como NPM y Composer.
+                    </p>
+                </div>
+                <div className='logo_container'>
+                    <IoLogoCss3 id='css_logo' className='logo' />
+                    <FaNode id='node_logo' className='logo' />
+                    <SiTailwindcss id='tw_logo' className='logo' />
+                    <SiPrisma id='prisma_logo' className='logo' />
+                    <IoLogoJavascript id='js_logo' className='logo' />
+                    <FaLaravel id='laravel_logo' className='logo' />
+                </div>
+            </div>
         </article>
     )
 }
